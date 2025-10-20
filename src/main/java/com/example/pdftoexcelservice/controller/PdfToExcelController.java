ba59e55d680baedad91a3da96cca52c72179205e
ruq94 <ruquaiatabassum1@gmail.com>
2025-10-21 00:15:57 +0530

    initial changes

--- src/main/java/com/example/pdftoexcelservice/controller/PdfToExcelController.java
+++ src/main/java/com/example/pdftoexcelservice/controller/PdfToExcelController.java
@@ -23,40 +23,15 @@ import java.nio.file.Paths;
 @Slf4j
 
 public class PdfToExcelController {
-    private final PdfOcrService pdfOcrService;
     BatchProcessingService batchProcessingService;
 
-  /*  @PostMapping(path = "/extract", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-
- public Mono<ResponseEntity<?>> extractToText(@RequestPart("file") Mono<FilePart> filePartMono) {
-       return filePartMono.flatMap(filePart -> {
-            if (filePart == null) {
-                log.info("No file uploaded or file is empty");
-                return Mono.just(ResponseEntity.badRequest().body("File is required and cannot be empty"));
-            }
-            try {
-                return pdfOcrService.extractDataFromFile(filePart)
-                        .map(result -> {
-                            log.info("File processed successfully: {}", filePart.filename());
-                            return ResponseEntity.ok(result);
-                        });
-            } catch (Exception e) {
-                log.error("Error extracting text from PDF", e);
-                return Mono.just(ResponseEntity
-                        .status(500)
-                        .body("Error extracting text: " + e.getMessage()));
-            }
-        });}
- */
     @PostMapping("/processFolder")
     public ResponseEntity processFolder(
             @RequestParam("folderName") String folderName) {
-         String pdfBaseDir = "C:/voterData/";  // hardcoded or via @Value
+         String pdfBaseDir = "C:/voterData/";  // hardcoded
 
-        // And an output base directory
        String outputBaseDir = "C:/voterData/";
 
-                    // sanitize folderName, avoid path traversal
                     if (folderName.contains("..") || folderName.contains("/") || folderName.contains("\\")) {
                         return ResponseEntity.badRequest().body(null);
                     }
@@ -67,7 +42,6 @@ public class PdfToExcelController {
                         return ResponseEntity.badRequest().body(null);
                     }
 
-                    // Call the service method
                     BatchProcessingResult result = batchProcessingService.processFolder(
                             folderPath.toString(), outputBaseDir);
 
